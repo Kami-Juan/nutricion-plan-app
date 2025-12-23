2872c0a4222140f9659e502a457c2de68c7836e0
Juan de Dios Canche Cen <juan1910@live.com.mx>
2025-12-23 10:37:21 -0600

    feat(k2): added new app (#7)
    * feat(k2): added new app
    
    * chore: updated pnpm.lock

---
+++ apps/k2/src/app/page.tsx
@@ -0,0 +1,18 @@
+import { Footer } from "@/components/landing-page/footer";
+import { Header } from "@/components/landing-page/header";
+import { Title } from "@/components/landing-page/title";
+
+export default async function Home() {
+  return (
+    <div className="min-h-screen bg-background relative pb-26">
+      <Header />
+      <main className="container mx-auto px-4 py-6 md:py-10">
+        <div className="mx-auto max-w-[980px] text-center">
+          <Title />
+        </div>
+        <div className="mx-auto max-w-[980px]">Menus</div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
